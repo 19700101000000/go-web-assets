fe7eef0982a76f97735f2327180d22066bb55aee
asuka y <yagi.asuka.0x06@gmail.com>
2019-07-08 16:50:50 +0900

    Fix main.go

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
-  "github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin"
+	"io"
+	"log"
+	"os"
 )
 
+func init() {
+	gin.SetMode(gin.ReleaseMode)
+	gin.DisableConsoleColor()
+	ginFile, err := os.OpenFile("webserver.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	gin.DefaultWriter = io.MultiWriter(ginFile)
+}
 
 func main() {
-  r := gin.Default()
+	r := gin.Default()
 
-  r.Static("/", "./assets")
+	r.Static("/", "./assets")
 
-  r.Run(":8081")
+	r.Run(":8081")
 }
